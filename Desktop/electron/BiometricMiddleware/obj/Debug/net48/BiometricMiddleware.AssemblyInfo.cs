983941031e0487a376e0f35729885435f949118f
Draxel1426 <paulx098@gmail.com>
2026-01-20 15:11:49 -0600

    GitIgnore para el middleware

--- Desktop/electron/BiometricMiddleware/obj/Debug/net48/BiometricMiddleware.AssemblyInfo.cs
+++ Desktop/electron/BiometricMiddleware/obj/Debug/net48/BiometricMiddleware.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiometricMiddleware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+b04ee2cf3725d8bbdcbd3e40b277176c42bb2eb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+cbb1daeb827df1261d244455ceb49f59db44a1f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiometricMiddleware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiometricMiddleware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
